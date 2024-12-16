5f13dbe7a7987ce6df0090e4027aa2c610679a36
Oyebode Precious <preckidtech@gmail.com>
2024-12-17 00:37:01 +0100

    responsive for hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           src={wave}
           alt="wave"
         />
-        <section className="hero-content  py-[100px] ml-10">
+        <section className="max-sm:hidden hero-content  py-[100px] ml-10">
           <section className="grid w-2/6 bg-white  p-7 rounded-md">
             <h1 className="text-4xl mb-7 font-semibold">
               Ready to declutter <br /> your closet?
@@ -22,6 +22,20 @@ const Hero = () => {
               Learn how it works
             </a>
           </section>
+        </section>
+      </section>
+      {/* hero content on small device */}
+      <section className="md:hidden lg:hidden hero-content mx-5">
+        <section className="grid w-full text-center bg-white rounded-md">
+          <h1 className="text-2xl mb-7 font-semibold">
+            Ready to declutter your closet?
+          </h1>
+          <button className="bg-[rgb(0,118,131)] p-3 rounded-md text-white font-bold mb-3">
+            Sell now
+          </button>
+          <a className="text-[rgb(0,118,131)] font-bold text-md" href="">
+            Learn how it works
+          </a>
         </section>
       </section>
     </>
